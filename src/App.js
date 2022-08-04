29087c0aca4ee0bb6737a2a85938f85d12a87e61
Jaron Cummings <jkc5020@rit.edu>
2022-08-04 18:14:22 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './Navigation/Navbar.js'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
